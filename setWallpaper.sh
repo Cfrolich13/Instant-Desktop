69852a7e2298d0e3e0de739264d1cdd6236f3745
Cfrolich <cfro1212@gmail.com>
2023-03-31 18:15:34 -0500

    Revert "Replaced "/home/jovyan" with "$HOME""
    This reverts commit 50cd40b4f4431ac1a718f3c45caf460330b2427a.

--- setWallpaper.sh
+++ setWallpaper.sh
@@ -1,2 +1,2 @@
 sleep 5
-xfconf-query -c xfce4-desktop -p  /backdrop/screen0/monitorVNC-0/workspace0/last-image -s $HOME/Wallpaper/mountainsWallpaper.png
+xfconf-query -c xfce4-desktop -p  /backdrop/screen0/monitorVNC-0/workspace0/last-image -s /home/jovyan/Wallpaper/mountainsWallpaper.png

fe5634fda4cc3f31a8ab9696c357727b4e62ce08
Cfrolich13 <80122013+Cfrolich13@users.noreply.github.com>
2023-04-18 12:39:38 -0500

    Update setPanel.sh
    Return mouse to original position after clicking to avoid inconveniencing the user.

--- setPanel.sh
+++ setPanel.sh
@@ -1,3 +1,3 @@
-xdotool sleep 3 mousemove 960 527 click 1 sleep 1 key Return
+xdotool sleep 3 mousemove 960 527 click 1 sleep 0.01 mousemove restore sleep 1 key Return
 sleep 2
 xfconf-query -c xfce4-panel -p /panels/panel-1/size -s 21

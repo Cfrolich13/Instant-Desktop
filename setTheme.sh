d35ac14c71284b24325424d07818fdfae1737b48
Cfrolich13 <80122013+Cfrolich13@users.noreply.github.com>
2023-04-24 16:27:31 +0000

    Make theme directory and fix Dark theme url.

--- setTheme.sh
+++ setTheme.sh
@@ -1,8 +1,10 @@
 #If the script doesn't have what it takes, give up and run!
 set -e
+#Makes downloads and theme directories
 mkdir -p $HOME/Downloads
+mkdir -p $HOME/.themes
 #Downloads theme from the lovely guthib
-curl -o '/home/jovyan/Downloads/WhiteSur-Dark-solid-42-0.tar.xz'  https://raw.githubusercontent.com/vinceliuice/WhiteSur-gtk-theme/master/release/WhiteSur-Light-solid-42-0.tar.xz
+curl -o '/home/jovyan/Downloads/WhiteSur-Dark-solid-42-0.tar.xz'  https://raw.githubusercontent.com/vinceliuice/WhiteSur-gtk-theme/master/release/WhiteSur-Dark-solid-42-0.tar.xz
 #Extracts theme to themes folder to install
 tar -xf '/home/jovyan/Downloads/WhiteSur-Dark-solid-42-0.tar.xz' -C '/home/jovyan/.themes'
 #At last, the transformation is complete! Once the theme is set, we'll be unstoppable!!!!

e713a2e281683cb0736c46b27b9697174358598f
Cfrolich13 <80122013+Cfrolich13@users.noreply.github.com>
2023-04-25 11:44:12 -0500

    Update firefoxReset.sh
    Remove loop, as it's broken and unnecessary

--- firefoxReset.sh
+++ firefoxReset.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #Ready!
 sleep 7
-for (( i=0; i<2; ++i)); do
-    #Fire!
-    firefox &
-    #Hit!
-    sleep 5
-    #Reload!
-    pkill firefox
-    sleep 5
-done
+#Fire!
+firefox &
+#Hit!
+sleep 5
+#Reload!
+pkill firefox
+sleep 5
 exit

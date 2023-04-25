5678b5555a2bf51b6e60a5c3ee4ee3e4edd31be3
Cfrolich13 <80122013+Cfrolich13@users.noreply.github.com>
2023-04-25 13:44:08 +0000

    Update theme URL

--- setTheme.sh
+++ setTheme.sh
@@ -4,8 +4,8 @@ set -e
 mkdir -p $HOME/Downloads
 mkdir -p $HOME/.themes
 #Downloads theme from the lovely guthib
-curl -o '/home/jovyan/Downloads/WhiteSur-Dark-solid-42-0.tar.xz'  https://raw.githubusercontent.com/vinceliuice/WhiteSur-gtk-theme/master/release/WhiteSur-Dark-solid-42-0.tar.xz
+curl -o '/home/jovyan/Downloads/WhiteSur-Dark-42-0.tar.xz'  https://raw.githubusercontent.com/vinceliuice/WhiteSur-gtk-theme/master/release/WhiteSur-Dark-42-0.tar.xz
 #Extracts theme to themes folder to install
-tar -xf '/home/jovyan/Downloads/WhiteSur-Dark-solid-42-0.tar.xz' -C '/home/jovyan/.themes'
+tar -xf '/home/jovyan/Downloads/WhiteSur-Dark-42-0.tar.xz' -C '/home/jovyan/.themes'
 #At last, the transformation is complete! Once the theme is set, we'll be unstoppable!!!!
-xfconf-query -c xsettings -p /Net/ThemeName -s WhiteSur-Dark-solid
+xfconf-query -c xsettings -p /Net/ThemeName -s WhiteSur-Dark

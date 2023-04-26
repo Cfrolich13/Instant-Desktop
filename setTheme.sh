7755c84b628630c8902c2f5d4bc56f9155f6bd1b
Cfrolich13 <80122013+Cfrolich13@users.noreply.github.com>
2023-04-26 11:11:26 -0500

    Update setTheme.sh
    Uses -L flag for curl to potentially fix failed download

--- setTheme.sh
+++ setTheme.sh
@@ -4,7 +4,7 @@ set -e
 mkdir -p $HOME/Downloads
 mkdir -p $HOME/.themes
 #Downloads theme from the lovely guthib
-curl -o '/home/jovyan/Downloads/WhiteSur-Dark-42-0.tar.xz'  https://raw.githubusercontent.com/vinceliuice/WhiteSur-gtk-theme/master/release/WhiteSur-Dark-42-0.tar.xz
+curl -L -o '/home/jovyan/Downloads/WhiteSur-Dark-42-0.tar.xz'  https://raw.githubusercontent.com/vinceliuice/WhiteSur-gtk-theme/master/release/WhiteSur-Dark-42-0.tar.xz
 #Extracts theme to themes folder to install
 tar -xf '/home/jovyan/Downloads/WhiteSur-Dark-42-0.tar.xz' -C '/home/jovyan/.themes'
 #At last, the transformation is complete! Once the theme is set, we'll be unstoppable!!!!

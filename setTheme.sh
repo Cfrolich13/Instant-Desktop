b8fb08804aeef33138ac671bc790fa76d867e10e
Cfrolich13 <80122013+Cfrolich13@users.noreply.github.com>
2023-04-26 11:14:18 -0500

    Update setTheme.sh
    Fix broken url

--- setTheme.sh
+++ setTheme.sh
@@ -4,9 +4,9 @@ set -e
 mkdir -p $HOME/Downloads
 mkdir -p $HOME/.themes
 #Downloads theme from the lovely guthib
-curl -L -o '/home/jovyan/Downloads/WhiteSur-Dark-42-0.tar.xz'  https://raw.githubusercontent.com/vinceliuice/WhiteSur-gtk-theme/master/release/WhiteSur-Dark-42-0.tar.xz
+curl -L -o '/home/jovyan/Downloads/WhiteSur-Dark-44-0.tar.xz'  https://raw.githubusercontent.com/vinceliuice/WhiteSur-gtk-theme/master/release/WhiteSur-Dark-44-0.tar.xz
 #Extracts theme to themes folder to install
-tar -xf '/home/jovyan/Downloads/WhiteSur-Dark-42-0.tar.xz' -C '/home/jovyan/.themes'
+tar -xf '/home/jovyan/Downloads/WhiteSur-Dark-44-0.tar.xz' -C '/home/jovyan/.themes'
 #At last, the transformation is complete! Once the theme is set, we'll be unstoppable!!!!
 xfconf-query -c xsettings -p /Net/ThemeName -s WhiteSur-Dark
 xfconf-query -c xfwm4 -p     /general/theme -s WhiteSur-Dark

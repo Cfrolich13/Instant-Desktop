4cefc04e84628781ea2eeca398b4e3b97596fb33
Cfrolich13 <80122013+Cfrolich13@users.noreply.github.com>
2023-04-27 16:37:43 +0000

    Add icon theme

--- setTheme.sh
+++ setTheme.sh
@@ -5,8 +5,12 @@ mkdir -p $HOME/Downloads
 mkdir -p $HOME/.themes
 #Downloads theme from the lovely guthib
 curl -L -o '/home/jovyan/Downloads/WhiteSur-Dark-44-0.tar.xz'  https://raw.githubusercontent.com/vinceliuice/WhiteSur-gtk-theme/master/release/WhiteSur-Dark-44-0.tar.xz
-#Extracts theme to themes folder to install
+#Icons
+git clone https://github.com/vinceliuice/Colloid-icon-theme.git
+#Installs themes
 tar -xf '/home/jovyan/Downloads/WhiteSur-Dark-44-0.tar.xz' -C '/home/jovyan/.themes'
+'/home/jovyan/Colloid-icon-theme/install.sh'
 #At last, the transformation is complete! Once the theme is set, we'll be unstoppable!!!!
-xfconf-query -c xsettings -p /Net/ThemeName -s WhiteSur-Dark
-xfconf-query -c xfwm4 -p     /general/theme -s WhiteSur-Dark
+xfconf-query -c xsettings -p /Net/ThemeName     -s WhiteSur-Dark
+xfconf-query -c xfwm4     -p /general/theme     -s WhiteSur-Dark
+xfconf-query -c xsettings -p /Net/IconThemeName -s Colloid-dark

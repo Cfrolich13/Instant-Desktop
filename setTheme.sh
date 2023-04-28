fcda212ed24a9c95201494719eb79cea367509a6
Cfrolich13 <80122013+Cfrolich13@users.noreply.github.com>
2023-04-28 16:34:07 +0000

    Replaced Colloid icon theme with Kora

--- setTheme.sh
+++ setTheme.sh
@@ -3,14 +3,15 @@ set -e
 #Makes downloads and theme directories
 mkdir -p $HOME/Downloads
 mkdir -p $HOME/.themes
+mkdir -p $HOME/.local/share/icons
 #Downloads theme from the lovely guthib
 curl -L -o '/home/jovyan/Downloads/WhiteSur-Dark-44-0.tar.xz'  https://raw.githubusercontent.com/vinceliuice/WhiteSur-gtk-theme/master/release/WhiteSur-Dark-44-0.tar.xz
 #Icons
-git clone https://github.com/vinceliuice/Colloid-icon-theme.git
+git clone https://github.com/bikass/kora.git
 #Installs themes
 tar -xf '/home/jovyan/Downloads/WhiteSur-Dark-44-0.tar.xz' -C '/home/jovyan/.themes'
-#'/home/jovyan/Colloid-icon-theme/install.sh'
+cp /home/jovyan/kora/kora /home/jovyan/.local/share/icons
 #At last, the transformation is complete! Once the theme is set, we'll be unstoppable!!!!
 xfconf-query -c xsettings -p /Net/ThemeName     -s WhiteSur-Dark
 xfconf-query -c xfwm4     -p /general/theme     -s WhiteSur-Dark
-#xfconf-query -c xsettings -p /Net/IconThemeName -s Colloid-dark
+xfconf-query -c xsettings -p /Net/IconThemeName -s Kora

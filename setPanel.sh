5887c7c345f86813945d479a9256fb12e72a16cd
Cfrolich13 <80122013+Cfrolich13@users.noreply.github.com>
2023-05-01 16:27:50 +0000

    Add button layout set to panel script

--- setPanel.sh
+++ setPanel.sh
@@ -1,3 +1,6 @@
+#Top panel
 xdotool sleep 3 mousemove 960 527 click 1 sleep 0.01 mousemove restore sleep 1 key Return
 sleep 2
 xfconf-query -c xfce4-panel -p /panels/panel-1/size -s 21
+#WM Button Layout
+xfconf-query -c xfwm4 -p /general/button_layout -s "CHM|"
